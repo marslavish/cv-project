d02271b18bcb265da90173cd6ec57cb1c88e8792
marslavish <uniqueye608@gmail.com>
2020-10-14 17:47:51 +0800

    Updates

---
+++ precache-manifest.8c1e0b33fa8a8d9afc6d0906e2f4c6c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45724dff2d20912ecbaee6e7c6ce6f4d",
+    "url": "/cv-project/index.html"
+  },
+  {
+    "revision": "449b213ef4a3aab23096",
+    "url": "/cv-project/static/css/main.114ccd27.chunk.css"
+  },
+  {
+    "revision": "9b402dab5c939c2e6f3d",
+    "url": "/cv-project/static/js/2.dca5188c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-project/static/js/2.dca5188c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449b213ef4a3aab23096",
+    "url": "/cv-project/static/js/main.02569945.chunk.js"
+  },
+  {
+    "revision": "1d802e09eb0bb136d206",
+    "url": "/cv-project/static/js/runtime-main.df1d416e.js"
+  }
+]);
